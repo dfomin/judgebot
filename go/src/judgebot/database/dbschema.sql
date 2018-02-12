9c881a1dc2b6861b7ad91db58a1ee28b9fb1a272
Dmitry Fomin <fomin.dm@gmail.com>
2018-02-12 15:04:06 +0200

    Added schema to sql.

--- go/src/judgebot/database/dbschema.sql
+++ go/src/judgebot/database/dbschema.sql
@@ -1,26 +1,26 @@
-CREATE DATABASE judgebot OWNER tstkbot;
+CREATE DATABASE judgebot OWNER judgebot;
 
-CREATE SCHEMA tstkbot AUTHORIZATION tstkbot;
+CREATE SCHEMA judgebot AUTHORIZATION judgebot;
 
-CREATE TABLE users (
+CREATE TABLE judgebot.users (
     id serial NOT NULL,
     telegram_id integer UNIQUE NOT NULL,
 
     CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE judge_phrases (
+CREATE TABLE judgebot.judge_phrases (
     id serial NOT NULL,
     phrase text UNIQUE,
 
     CONSTRAINT judge_phrases_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE votes (
+CREATE TABLE judgebot.votes (
     user_id integer NOT NULL,
     judge_phrase_id integer NOT NULL,
 
     CONSTRAINT vote_pkey PRIMARY KEY (user_id, judge_phrase_id),
-    CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT judge_phrase_id_fkey FOREIGN KEY (judge_phrase_id) REFERENCES judge_phrases (id)
+    CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES judgebot.users (id),
+    CONSTRAINT judge_phrase_id_fkey FOREIGN KEY (judge_phrase_id) REFERENCES judgebot.judge_phrases (id)
 );
