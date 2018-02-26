b4bf8f450707a135a774fd43f6871a889e470d11
Pavel Tatarintsev <tatarincev.pavel@gmail.com>
2018-02-26 17:41:00 +0300

    Moved sql queries to separate file

--- database/database.go
+++ database/database.go
@@ -9,13 +9,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const judgeListQuery = `SELECT phrase FROM judgebot.judge_phrases`
-const phraseInsertQuery = `INSERT INTO judgebot.judge_phrases (phrase) VALUES ($1) RETURNING id`
-const voteForPhraseQuery = `INSERT INTO judgebot.votes (vote, user_id, judge_phrase_id) VALUES ($1, $2, $3)`
-const userIDQuery = `SELECT id FROM judgebot.users WHERE telegram_id = $1`
-const phraseIDQuery = `SELECT id FROM judgebot.judge_phrases WHERE phrase = $1`
-const userInsertQuery = `INSERT INTO judgebot.users (telegram_id) VALUES ($1) RETURNING id`
-
 type Controller struct {
 	DataBase *sql.DB
 }
@@ -39,12 +32,12 @@ func Init() *Controller {
 
 func (dbc *Controller) getUserID(telegramID int) int {
 	var userID int
-	err := dbc.DataBase.QueryRow(userIDQuery, telegramID).Scan(&userID)
+	err := dbc.DataBase.QueryRow(getUserIDQuery(), telegramID).Scan(&userID)
 	if err != nil {
 		if err != sql.ErrNoRows {
 			log.Fatal(err)
 		}
-		err = dbc.DataBase.QueryRow(userInsertQuery, telegramID).Scan(&userID)
+		err = dbc.DataBase.QueryRow(getUserInsertQuery(), telegramID).Scan(&userID)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -55,12 +48,12 @@ func (dbc *Controller) getUserID(telegramID int) int {
 
 func (dbc *Controller) getPhraseID(phrase string) int {
 	var phraseID int
-	err := dbc.DataBase.QueryRow(phraseIDQuery, phrase).Scan(&phraseID)
+	err := dbc.DataBase.QueryRow(getPhraseIDQuery(), phrase).Scan(&phraseID)
 	if err != nil {
 		if err != sql.ErrNoRows {
 			log.Fatal(err)
 		}
-		err = dbc.DataBase.QueryRow(phraseInsertQuery, phrase).Scan(&phraseID)
+		err = dbc.DataBase.QueryRow(getPhraseInsertQuery(), phrase).Scan(&phraseID)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -72,14 +65,14 @@ func (dbc *Controller) getPhraseID(phrase string) int {
 func (dbc *Controller) JudgeVote(telegramID int, phrase string, vote bool) {
 	userID := dbc.getUserID(telegramID)
 	phraseID := dbc.getPhraseID(phrase)
-	_, err := dbc.DataBase.Query(voteForPhraseQuery, vote, userID, phraseID)
+	_, err := dbc.DataBase.Query(getVoteForPhraseQuery(), vote, userID, phraseID)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func (dbc *Controller) JudgeList() []string {
-	rows, err := dbc.DataBase.Query(judgeListQuery)
+	rows, err := dbc.DataBase.Query(getJudgeListQuery())
 	if err != nil {
 		log.Fatal(err)
 	}
