cb071f321cf1a0a549c6f97c80cb04c53122e2ca
Dmitry Fomin <fomin.dm@gmail.com>
2022-03-15 18:30:33 +0200

    Fetching phrases

--- judgebot/main.py
+++ judgebot/main.py
@@ -1,7 +1,11 @@
+from typing import List
+
+import psycopg
 from telegram import Update
 from telegram.ext import Updater, Dispatcher, CallbackContext, CommandHandler
 
-from judgebot.private import TOKEN
+from judgebot.private import TOKEN, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_USER
+from judgebot.sql.queries import get_judge_list_query
 
 
 def start(update: Update, context: CallbackContext):
@@ -13,7 +17,21 @@ def help(update: Update, context: CallbackContext):
 
 
 def judge(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="😐")
+    # context.bot.send_message(chat_id=update.effective_chat.id, text="😐")
+    phrases = applicable_judge_list(update.effective_chat.id, 0)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=phrases[0])
+
+
+def applicable_judge_list(chat_id: int, chat_members_count: int) -> List[str]:
+    phrases = get_sorted_judge_phrases(chat_id, chat_members_count)
+    return phrases
+
+
+def get_sorted_judge_phrases(chat_id: int, chat_members_count: int) -> List[str]:
+    with psycopg.connect(f"dbname={DATABASE_NAME} password={DATABASE_PASSWORD} user={DATABASE_USER} sslmode=disable") as conn:
+        with conn.cursor() as cur:
+            cur.execute(get_judge_list_query(), f"{chat_id}")
+            return cur.fetchall()
 
 
 def main():
